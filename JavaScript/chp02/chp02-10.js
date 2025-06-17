fa76414464fe1036eceaf2a773974b77c09f0729
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-06-17 01:34:06 -0400

    Using String Operators activity.
    # Demonstrates creating and storing variables using srting operators to provide result output.
    
    #  Imports string values from JS to output into HTML for a welcome message.

---
+++ JavaScript/chp02/chp02-10.js
@@ -0,0 +1,7 @@
+var greeting = "Howdy ";
+var name = "Molly";
+
+var welcomeMessage = greeting + name + "!";
+
+var el = document.getElementById("welcome-message");
+el.textContent = welcomeMessage;
