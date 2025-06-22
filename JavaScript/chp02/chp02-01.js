60ca64573758157899dbfff5edd28cffc8d08b51
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-06-22 17:58:08 -0400

    chp02-01 Added buttons displaying/popup html/javascript code for activity.
    # Divides html and javascript buttons offering code sources for the visual code demonstration between left and right bottom of the page.
    
    # When user selects "show html" or "show javascript" they are presented a popup to read the source code used to complete and display for better understanding.
    
    # The javascript source code will be demonstrated positioned above the html when both are activated.
    
    # This led to further exercise and discovery using javascript to present popup through use of creating a call to html atribute returning display/data output based on a click event.

--- JavaScript/chp02/chp02-01.js
+++ JavaScript/chp02/chp02-01.js
@@ -9,7 +9,13 @@ total = price * quantity;
 var el = document.getElementById("cost");
 el.textContent = '$' + total;
 
-document.querySelector('.button-link-js').addEventListener("click", function () {
-    var popup = document.getElementById("js-popup");
-    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+document.querySelectorAll(".button-link").forEach(function (btn) {
+    btn.addEventListener("click", function() {
+        var popupSelector = btn.getAttribute("data-popup");
+        var popup = document.querySelector(popupSelector);
+        if (popup) {
+            popup.style.display = (popup.style.display === "block") ? "none" : "block";
+        }
+    });
 });
+
