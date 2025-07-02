bfc5cad40faadae373dccfafe452ad97e1cec38b
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-07-02 16:00:27 -0400

    Chp05-01: Selecting Elements Using ID Attribute.
    # demonstrates celecting the first list item be it's id attribute.
    # stores the location of the id attribute into a variable.
    # uses variable storing id location reference to change the class attribute of the element in the DOM.

---
+++ JavaScript/chp05/chp05-01.js
@@ -0,0 +1,7 @@
+// Selecting Elements Using ID Attributes
+
+// Selects the element and stores it in a variable
+var el = document.getElementById('one');
+
+// changes the value of the class attribute
+el.className = 'cool';
