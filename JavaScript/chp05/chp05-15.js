cc5109dacbe9b0511644326ab93d017638192967
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-07-05 23:47:46 -0400

    Chp05-15: Removing Attributes.
    # DOM query gets an element by id and stores reference to the element in a variable named firstItem.
    # if statement uses .hasAttribute() method as a condition for firstItem.
    # condition is met, code inside curly braces removes the class from the element.

---
+++ JavaScript/chp05/chp05-15.js
@@ -0,0 +1,6 @@
+// Removing Attributes
+
+var firstItem = document.getElementById('one'); // get the first item
+if ( firstItem.hasAttribute('class')) { // if it has a class attribute
+    firstItem.removeAttribute('class'); // remove its class attribute
+}
